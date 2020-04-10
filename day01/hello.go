54ec408b6547cbb66e33c178d8fca4a74522c97e
ispy1 <mrliujiawei@foxmail.com>
2020-04-10 18:16:51 +0800

    1

--- day01/hello.go
+++ day01/hello.go
@@ -5,7 +5,12 @@ import "fmt"
 func main() {
 	fmt.Println("hello world")
 	fmt.Println(syaHello("world"))
+	fmt.Println(addition(2, 1))
 }
 func syaHello(s string) string {
 	return "Hello " + s
+}
+
+func addition(x int, y int) int {
+	return x + y
 }
