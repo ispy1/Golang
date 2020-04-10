b3098e4d81e5a58b45a29efa1d3e41087def718b
ispy1 <mrliujiawei@foxmail.com>
2020-04-10 21:34:47 +0800

    变量

--- day01/var.go
+++ day01/var.go
@@ -12,12 +12,15 @@ func main() {
 	var c float64 = 3.14 //浮点数
 	var w string = "ccc" //字符串
 	var t [5]string      // 字符数组
+	var y [2]int         //int数组
+	y[1] = 1
+	y[0] = 0
 	t[1] = "bvbb"
 	t[2] = "ccc"
 	t[3] = "111"
 	t[4] = "bbbbb"
 	t[0] = "'wo shi'"
-
+	fmt.Println(y)
 	fmt.Println(t[0])
 	fmt.Println(t)
 	fmt.Println("s")
