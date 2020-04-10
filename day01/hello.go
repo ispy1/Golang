9f5d4a95948ee1a2e541cc631cdd3a11cc9666bd
ispy1 <mrliujiawei@foxmail.com>
2020-04-10 21:16:48 +0800

    test

--- day01/hello.go
+++ day01/hello.go
@@ -1,16 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+
+	var b bool = true    //布尔
+	var a int = -3       //整数
+	var c float64 = 3.14 //浮点数
+	var w string = "ccc" //字符串
+	var t [5]string      // 字符数组
+	t[1] = "bvbb"
+	t[2] = "ccc"
+	t[3] = "111"
+	t[4] = "bbbbb"
+	t[0] = "'wo shi'"
+
+	fmt.Println(t[0])
+	fmt.Println(t)
+	fmt.Println("s")
+	fmt.Println(b)
+	fmt.Println(a)
+	fmt.Println(addition(c, 2))
+	fmt.Println(w)
 	fmt.Println("hello world")
 	fmt.Println(syaHello("world"))
 	fmt.Println(addition(2, 1))
+
 }
 func syaHello(s string) string {
 	return "Hello " + s
 }
 
-func addition(x int, y int) int {
+func addition(x float64, y float64) float64 {
 	return x + y
 }
