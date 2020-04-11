5a39dedbdf0c11330c7768426e8e1ba8abcef947
ispy1 <mrliujiawei@foxmail.com>
2020-04-11 22:30:13 +0800

    test

--- golang/day02/day02.go
+++ golang/day02/day02.go
@@ -7,11 +7,20 @@ import (
 	"strconv"
 )
 
+func ShowMemAddr(x *string) {
+	fmt.Println(x)
+	return
+}
+
+// & 指针
 func main() {
 	var s bool = true
 	fmt.Println(reflect.TypeOf(s))
 	var b string = strconv.FormatBool(s)
 	fmt.Println(reflect.TypeOf(b))
 	fmt.Println(&b)
+	var t string = "aaaa"
+	ShowMemAddr(&t)
+	fmt.Println(*&t)
 
 }
